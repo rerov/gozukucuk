67c2da305536c6d778ca1caf3b5c0a6ae1a52752
Meliha Manav <g.melihamanav@gmail.com>
2023-08-04 17:48:44 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,101 +1,76 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars
+} from "@fortawesome/free-solid-svg-icons";
+
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <>
+      <div className={styles.banner}>
+        <div className={styles.topHeader}>
+          <div>
+            <a href='#'>
+              EN
+            </a>
+            <a href='#'>
+              TR
+            </a>
+          </div>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
-      </main>
+        <video autoPlay={true} controls={false} muted={true} loop={true} className={styles.video} width="640" height="360">
+          <source src="/img/loop.mp4" type="video/mp4" />
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
+        </video>
+        <div className={styles.overlay}></div>
+        <div className={styles.header}>
 
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-      `}</style>
+          <div className={styles.headerMenu}>
+
+            <div className={styles.logo}>
+              <img src='/img/logo_home.gif'></img>
+
+            </div>
+            <div className={styles.rightMenu}>
+              <ul>
+                <li><a href='#'>NEWS</a></li>
+                <li><a href='#'>CONTACT</a></li>
+                <li><a href='#'>CASES</a></li>
+                <li><a href='#'>BLOGS</a></li>
+                <li><a href='#'>GET CONTACTED</a></li>
+              </ul>
+
+              <div className={styles.sideMenu}>
+                <a href='#'>
+                  <p>MENU </p>
+                  <FontAwesomeIcon
+                    icon={faBars}
+                  />
+                </a>
+              </div>
+            </div>
+
+          </div>
+
+          <div className={styles.heroHeading}>
+            <h1>YOUR RELIABLE <br></br>PARTNER </h1>
+            <h3>Quality & Precision</h3>
+          </div>
+          <div className={styles.bannerButton}>
+            <a href='#'>
+              <span>DISCOVER US</span>
+
+            </a>
+          </div>
+
+
+        </div>
+      </div>
+
+
 
       <style jsx global>{`
         html,
@@ -109,7 +84,10 @@ export default function Home() {
         * {
           box-sizing: border-box;
         }
+        a {
+          text-decoration: none; 
+        }
       `}</style>
-    </div>
+    </>
   )
 }
