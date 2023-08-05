7811bc2f094ae3b4a192e04cbc5f70e8f5f6d2f6
Meliha Manav <g.melihamanav@gmail.com>
2023-08-05 13:36:03 +0300

    menu added

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,98 @@ import {
   faBars
 } from "@fortawesome/free-solid-svg-icons";
 
+import { useState } from 'react';
 
 export default function Home() {
+
+  const [open, setOpen] = useState(false)
+
   return (
     <>
+      {
+        open && (
+          <>
+            <div onClick={() => setOpen(false)} className={`${styles.sideMenu} ${styles.closeButton}`}>
+              <a href='#'>
+                <p>MENU </p>
+                <FontAwesomeIcon
+                  icon={faBars}
+                />
+              </a>
+            </div>
+
+            <div className={styles.openMenu}>
+              <div style={{ background: "url('/img/services.jpg')", backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} className={styles.menuSection}>
+                <div className={styles.openMenuOverlay}>
+
+                </div>
+                <div className={styles.openMenuTexts}>
+                  <a className={styles.openMenuBaslik} href='#'>SERVICES</a>
+                  <ul>
+                    <li><a href='#'>CNC Machining</a></li>
+                    <li><a href='#'>CAD CAM</a></li>
+                    <li><a href='#'>Subcontracting</a></li>
+                    <li><a href='#'>Finishing</a></li>
+                    <li><a href='#'>Pack & Ship</a></li>
+                  </ul>
+                </div>
+              </div>
+
+              <div style={{ background: "url('/img/industry.jpg')", backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} className={styles.menuSection}>
+                <div className={styles.openMenuOverlay}>
+
+                </div>
+                <div className={styles.openMenuTexts}>
+                  <a className={styles.openMenuBaslik} href='#'>INDUSTRIES</a>
+                  <ul>
+                    <li><a href='#'>Industry</a></li>
+                    <li><a href='#'>Machinery</a></li>
+                    <li><a href='#'>Medical</a></li>
+                    <li><a href='#'>Railways</a></li>
+                    <li><a href='#'>Other</a></li>
+                  </ul>
+                </div>
+              </div>
+
+              <div style={{ background: "url('/img/services.jpg')", backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} className={styles.menuSection}>
+                <div className={styles.openMenuOverlay}>
+
+                </div>
+                <div className={styles.openMenuTexts}>
+                  <a className={styles.openMenuBaslik} href='#'>QUALITY</a>
+                  <ul>
+                    <li><a href='#'>Certification</a></li>
+                    <li><a href='#'>Measuring Lab</a></li>
+                    <li><a href='#'>Quality Control</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div style={{ background: "url('/img/industry.jpg')", backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} className={styles.menuSection}>
+                <div className={styles.openMenuOverlay}>
+
+                </div>
+                <div className={styles.openMenuTexts}>
+                  <a className={styles.openMenuBaslik} href='#'>ABOUT US</a>
+                  <ul>
+                    <li><a href='#'>History</a></li>
+                    <li><a href='#'>News</a></li>
+                    <li><a href='#'>Career</a></li>
+                    <li><a href='#'>References</a></li>
+                  </ul>
+                </div>
+              </div>
+              <div style={{ background: "url('/img/services.jpg')", backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} className={styles.menuSection}>
+                <div className={styles.openMenuOverlay}>
+
+                </div>
+                <div className={styles.openMenuTexts}>
+                  <a className={styles.openMenuBaslik} href='#'>CONTACT</a>
+                </div>
+              </div>
+            </div>
+          </>
+        )
+      }
       <div className={styles.banner}>
         <div className={styles.topHeader}>
           <div>
@@ -37,7 +125,7 @@ export default function Home() {
                 <li><a href='#'>BLOGS</a></li>
                 <li><a href='#'>GET CONTACTED</a></li>
               </ul>
-              <div className={styles.sideMenu}>
+              <div onClick={() => setOpen(true)} className={styles.sideMenu}>
                 <a href='#'>
                   <p>MENU </p>
                   <FontAwesomeIcon
@@ -58,10 +146,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
-
-
-
       <div className={styles.container}>
         <div className={styles.baslik}>
           <h2>PRECISION ENGINEERING SOLUTIONS</h2>
@@ -78,8 +162,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
-
 
       <style jsx global>{`
         html,
