a6c487fe918ab63301b59f1068682faca753cb0b
Meliha Manav <g.melihamanav@gmail.com>
2023-08-05 11:09:46 +0300

    engineering solutions

--- pages/index.js
+++ pages/index.js
@@ -19,20 +19,15 @@ export default function Home() {
               TR
             </a>
           </div>
-
         </div>
         <video autoPlay={true} controls={false} muted={true} loop={true} className={styles.video} width="640" height="360">
           <source src="/img/loop.mp4" type="video/mp4" />
-
         </video>
         <div className={styles.overlay}></div>
         <div className={styles.header}>
-
           <div className={styles.headerMenu}>
-
             <div className={styles.logo}>
               <img src='/img/logo_home.gif'></img>
-
             </div>
             <div className={styles.rightMenu}>
               <ul>
@@ -42,7 +37,6 @@ export default function Home() {
                 <li><a href='#'>BLOGS</a></li>
                 <li><a href='#'>GET CONTACTED</a></li>
               </ul>
-
               <div className={styles.sideMenu}>
                 <a href='#'>
                   <p>MENU </p>
@@ -52,9 +46,7 @@ export default function Home() {
                 </a>
               </div>
             </div>
-
           </div>
-
           <div className={styles.heroHeading}>
             <h1>YOUR RELIABLE <br></br>PARTNER </h1>
             <h3>Quality & Precision</h3>
@@ -62,11 +54,28 @@ export default function Home() {
           <div className={styles.bannerButton}>
             <a href='#'>
               <span>DISCOVER US</span>
-
             </a>
           </div>
+        </div>
+      </div>
 
 
+
+
+      <div className={styles.container}>
+        <div className={styles.baslik}>
+          <h2>PRECISION ENGINEERING SOLUTIONS</h2>
+          <h4>Advancing Production with Cutting-Edge <br /> CNC & CMM Technologies</h4>
+        </div>
+
+        <div className={styles.fotoGrid}>
+          <div className={styles.foto}>
+            <img src="/img/cnc.jpg" alt="Fotoğraf 1" />
+
+          </div>
+          <div className={styles.foto}>
+            <img src="/img/cmm.jpg" alt="Fotoğraf 2" />
+          </div>
         </div>
       </div>
 
